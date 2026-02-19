d2df483991a50e382722511f03d405ee82540cc1
itsorivera <its.orivera@gmail.com>
2026-02-18 23:31:45 -0500

    feat: adding app implementation and router apis

---
+++ src/adapter/rest.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends
+from config.dependencies import get_agent_investment_root
+import traceback
+
+router = APIRouter(prefix="/api/v1/investments",
+                   tags=["investments"])
+
+@router.post(
+        path="/query",
+        description="Endpoint to query the investment research agent with a natural language question about investors, companies, industries or news.")
+async def query_investment_agent(
+    question: str,
+    agent=Depends(get_agent_investment_root)
+    ):
+    try:
+        response = await agent.run(question)
+        return {"response": response}
+    except Exception as e:
+        traceback.print_exc()
+        return {"error": str(e)}
--- src/app.py
+++ src/app.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from graphrag-agentic-system!")
+from fastapi import FastAPI
+from adapter.rest import router as investments_router
 
+def create_app() -> FastAPI:
+    app = FastAPI()
+    app.include_router(investments_router)
+    
+    @app.get("/health")
+    async def health_check():
+        return {"status": "healthy",
+                "message": "The application is running smoothly!"}
 
-if __name__ == "__main__":
-    main()
+    @app.get("/health/liveness")
+    async def liveness_check():
+        return {"status": "alive"}
+    @app.get("/health/readiness")
+    async def readiness_check():
+        return {"status": "ready"}
+    return app
+
+app = create_app()
